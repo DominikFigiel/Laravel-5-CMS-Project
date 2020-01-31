5bfdcb65d937d92a7e7e1bf624252800e5c733f9
DominikFigiel <castro992@gmail.com>
2020-01-31 12:10:46 +0100

    Adding search functionality

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -10,10 +10,17 @@ use App\Tag;
 class WelcomeController extends Controller
 {
     public function index()
-    {
+    { 
+        $search = request()->query('search');
+        if ($search) {
+            $posts = Post::where('title', 'LIKE', "%{$search}%")->simplePaginate(2);
+        } else {
+            $posts = Post::simplePaginate(2);
+        }
+
         return view('welcome')
         ->with('categories', Category::all())
         ->with('tags', Tag::all())
-        ->with('posts', Post::simplePaginate(2));
+        ->with('posts', $posts);
     }
 }
