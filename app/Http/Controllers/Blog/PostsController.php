d72877e1f95865e85b589e10a4ce213266eae0a6
DominikFigiel <castro992@gmail.com>
2020-01-31 15:03:35 +0100

    Adding searching by tags and categories

--- app/Http/Controllers/Blog/PostsController.php
+++ app/Http/Controllers/Blog/PostsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Blog;
 
 use App\Post;
+use App\Category;
+use App\Tag;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -11,5 +13,39 @@ class PostsController extends Controller
     public function show(Post $post)
     {
         return view('blog.show')->with('post', $post);
+    }
+
+    public function category(Category $category)
+    {
+        $search = request()->query('search');
+        
+        if ($search) {
+            $posts = $category->posts()->where('title', 'LIKE', "%{$search}%")->simplePaginate(2);
+        } else {
+            $posts = $category->posts()->simplePaginate(2);
+        }
+
+        return view('blog.category')
+        ->with('category', $category)
+        ->with('posts', $category->posts()->simplePaginate(2))
+        ->with('categories', Category::all())
+        ->with('tags', Tag::all());
+    }
+
+    public function tag(Tag $tag)
+    {
+        $search = request()->query('search');
+        
+        if ($search) {
+            $tags = $tag->posts()->where('title', 'LIKE', "%{$search}%")->simplePaginate(2);
+        } else {
+            $tags = $tag->posts()->simplePaginate(2);
+        }
+
+        return view('blog.tag')
+        ->with('tag', $tag)
+        ->with('posts', $tag->posts()->simplePaginate(2))
+        ->with('categories', Category::all())
+        ->with('tags', Tag::all());;
     }
 }
